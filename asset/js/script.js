65de1e504a67eaed285df2c9acacf165506e5e72
anas12421 <anas161090@gmail.com>
2023-12-28 22:06:57 +0600

    demo

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,11 +1,11 @@
-$(window).scroll(function(){
-  if($(window).scrollTop() > 50){
-    $(".menu").addClass("kata")
-  }else{
-    $(".menu").removeClass("kata")
-  }
+// $(window).scroll(function(){
+//   if($(window).scrollTop() > 50){
+//     $(".menu").addClass("kata")
+//   }else{
+//     $(".menu").removeClass("kata")
+//   }
   
-})
+// })
 
 
 
@@ -44,34 +44,34 @@ $(function () {
   'use strict';
 
 
-  var navoff = $('.main_manu').offset().top;
+  // var navoff = $('.main_manu').offset().top;
 
-  $(window).scroll(function () {
+  // $(window).scroll(function () {
 
-    var scrolling = $(this).scrollTop();
+  //   var scrolling = $(this).scrollTop();
 
-    if (scrolling > navoff) {
-      $('.main_manu').addClass('manu_fixed')
-    } else {
-      $('.main_manu').removeClass('manu_fixed')
-    }
+  //   if (scrolling > navoff) {
+  //     $('.main_manu').addClass('manu_fixed')
+  //   } else {
+  //     $('.main_manu').removeClass('manu_fixed')
+  //   }
 
-  });
+  // });
 
 
-  var navoff = $('.img_manu').offset().top;
+  // var navoff = $('.img_manu').offset().top;
 
-  $(window).scroll(function () {
+  // $(window).scroll(function () {
 
-    var scrolling = $(this).scrollTop();
+  //   var scrolling = $(this).scrollTop();
 
-    if (scrolling > navoff) {
-      $('.img_manu').addClass('img_fix')
-    } else {
-      $('.img_manu').removeClass('img_fix')
-    }
+  //   if (scrolling > navoff) {
+  //     $('.img_manu').addClass('img_fix')
+  //   } else {
+  //     $('.img_manu').removeClass('img_fix')
+  //   }
 
-  });
+  // });
 
 
 
